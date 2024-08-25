dcff39bee168fc7905f7de7a661d96d58d47b31c
Sulkifli Asmunandar <kflmattukz@gmail.com>
2024-08-25 11:51:01 +0800

    update footer message
    change heart icon color to red 500

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,7 @@
 <footer class="font-semibold text-gray-800">
-  <p>Copyright &copy; darkzeref-mattz 2024, created by love &hearts;</p>
+	<p>
+		Copyright &copy; darkzeref-mattz 2024, created by love <span class="text-red-500">&hearts;</span
+		>
+	</p>
 </footer>
+
